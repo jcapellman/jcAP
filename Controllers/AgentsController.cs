8719c88b51ea9e806abeaecd6ff4a0b723bd1caa
Jarred Capellman <jcapellman@hotmail.com>
2025-11-23 19:53:46 -0800

    Added: Initial spec of what the Agents Controller would look like

---
+++ Controllers/AgentsController.cs
@@ -0,0 +1,52 @@
+﻿using jcAP.API.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcAP.API.Controllers
+{
+    public class AgentsController : BaseController
+    {
+        /// <summary>
+        /// Returns a list of Agents
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return [];
+        }
+        /// <summary>
+        /// Registers a new Agent
+        /// </summary>
+        [HttpPost]
+        public void Create()
+        {
+        }
+
+        /// <summary>
+        /// Updates an existing Agent
+        /// </summary>
+        [HttpPatch]
+        public void Update()
+        {
+        }
+
+        /// <summary>
+        /// Returns the metadata for a given agent
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public object Get(Guid id)
+        {
+            return new object();
+        }
+
+        /// <summary>
+        /// Deletes an Agent
+        /// </summary>
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+        }
+    }
+}
