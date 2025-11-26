9d65dd8ac924655520752217b1c4e4011bbb5251
Jarred Capellman <jcapellman@hotmail.com>
2025-11-25 16:12:01 -0800

    Added: MetricsDto

---
+++ Objects/About/MetricsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace jcAP.API.Objects.About
+{
+    public sealed record MetricsDto(
+        int ProcessId,
+        long WorkingSetBytes,
+        long VirtualMemoryBytes,
+        long PagedMemoryBytes,
+        int ThreadCount,
+        int HandleCount,
+        DateTime StartTimeUtc,
+        int ProcessorCount,
+        DateTime UtcNow);
+}
