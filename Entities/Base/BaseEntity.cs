bf9de7206c207f5c812c8416f6ec73b3490ba9a2
Jarred Capellman <jcapellman@hotmail.com>
2025-11-28 11:34:20 -0800

    Added: BaseEntity

---
+++ Entities/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace jcAP.API.Entities.Base
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public bool Active { get; set; }
+
+        public DateTime Created { get; set; }
+        
+        public DateTime Modified { get; set; }
+    }
+}
