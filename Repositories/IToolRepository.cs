9226e7ffdd23da1e1a1a3cb502619e641c088968
Jarred Capellman <jcapellman@hotmail.com>
2025-11-30 11:47:42 -0800

    Change: Updated IToolRepository

--- Repositories/IToolRepository.cs
+++ Repositories/IToolRepository.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using jcAP.API.Entities;
 using jcAP.API.Objects.Tools;
+using jcAP.API.Repositories.Common;
 
 namespace jcAP.API.Repositories
 {
-    public interface IToolRepository
+    public interface IToolRepository : IRepository<ToolEntity>
     {
-        Task<IEnumerable<ToolDto>> GetAllAsync(CancellationToken cancellationToken = default);
-        Task<ToolDto?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-        Task<ToolDto> CreateAsync(ToolDto dto, CancellationToken cancellationToken = default);
-        Task UpdateAsync(Guid id, ToolDto dto, CancellationToken cancellationToken = default);
+        Task<IEnumerable<ToolDto>> GetAllDtosAsync(CancellationToken cancellationToken = default);
+        Task<ToolDto?> GetDtoByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<ToolDto> CreateFromDtoAsync(ToolDto dto, CancellationToken cancellationToken = default);
+        Task UpdateFromDtoAsync(Guid id, ToolDto dto, CancellationToken cancellationToken = default);
         Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
     }
 }
